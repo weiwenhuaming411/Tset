17af6c507414a340a3950d5e2000df72b6431164
HuiPu <HuiPu@gmail.com>
2025-10-03 11:57:55 +0800

    fix: 修复

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const number = 1;
+let s = '12345'
 
-const fn = (a) => {
-  console.log('此代码还未格式化');
-};
-
-console.log('test commitLint');
-
-console.log('hello');
-
-console.log('再试一次');
+console.log(s)
